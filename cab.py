07b2b3d4830f9fdc6c2cc4b82485af4d53972df2
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-20 11:35:51 -0700

    Create cab.py

---
+++ cab.py
@@ -0,0 +1,7 @@
+ code = str(input("Enter bar string:\n"))
+print("+------------------+")
+i = 0
+while i<len(code):
+   print("|" + " " * (9-int(code[i])) + "#" * int(code[i]) + "#" * int(code[i+1]) + " " * (9-int(code[i+1])) + "|")
+   i = i +2
+print("+------------------+")
