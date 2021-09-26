e7c766130c98322c2e87acef1e41b4545b3fc461
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-25 17:27:48 -0700

    Update cab.py

--- cab.py
+++ cab.py
@@ -2,6 +2,6 @@
 print("+------------------+")
 i = 0
 while i<len(code):
-   print("|" + " " * (9-int(code[i])) + "#" * int(code[i]) + "#" * int(code[i+1]) + " " * (9-int(code[i+1])) + "|")
+   print("|"+" "*(9-int(code[i]))+"#"*int(code[i])+"#"*int(code[i+1])+" "*(9-int(code[i+1]))+"|")
    i = i +2
 print("+------------------+")
